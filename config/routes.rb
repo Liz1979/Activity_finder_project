77eceab01335aabb0ca95772aac9e85989829ba4
Elizabeth Dalton <you@example.com>
2017-10-11 11:23:36 +0100

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/help'
+
+  get 'activity_finder/index'
+
+  resources :users
+
+  root 'activity_finder#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
