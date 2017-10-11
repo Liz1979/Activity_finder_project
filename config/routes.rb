491c7843ded4f9b26204c1f545ef3652cb4d2834
Stephen Forrest <stephen.forrest@onthebeach.co.uk>
2017-10-11 14:49:07 +0100

    Added user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/help'
+  root to: "pages#home"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
-  get 'activity_finder/index'
-
-  resources :users
+  resources :users, except: [:new]
+  get '/signup', to: 'users#new', as: :new_user
 
   root 'activity_finder#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
